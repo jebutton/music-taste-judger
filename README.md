a58aa5835b9854e509145a6d6c4456f11182a904
jebutton <jacqueline@jebutton.com>
2023-08-23 10:49:52 -0700

    Uploading existing project files to the repo 8-23-2023

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # music-taste-judger
-This is just a silly project I'm working on as I learn C# for my job.
+
+## Description
+This is just a silly project I'm working on as I learn C# for my job, posted publicly with permission of my boss. 
+I wanted to create something that isn't just an isolated language exercise when studying C#, especially since I need to learn unit testing in NUnit.
+I come from a Java and Python background professionally, and I'm learning the nuances of C# as I go. It is definitely more than "Microsoft Java".
+This tool is designed to judge your choice of Synthwave artist. I may expand it to more genres in the future.
+
+***
+## Eventual Goals:
+- Implemening more Object Oriented Code like defining a class to represeent an artist.
+- Loading JSON or XML that artist definitions are stored in.
+- Writing tests for the tool using NUnit.
---
+++ music-taste-judger/music-taste-judger/Program.cs
@@ -0,0 +1,76 @@
+﻿/*
+ * This is a program created by Jacqueline Button 
+ * to allow her to work on implementing features of C#
+ * into a working program that isn't just a set of individual
+ * Exercises. This code will be improved overtime as she learns more.
+ */
+using static System.Console;
+class Program
+{
+    public static void Main()
+    {
+        WriteLine("Hello Cyberpunk Boy, Girl, or Hacker");
+        bool bandFound = false;
+        do
+        {
+            Write("Please Enter A Good Synthwave Artist: ");
+            string? band = ReadLine();
+            if (band == null)
+            {
+                WriteLine("Response is empty or null");
+            }
+            else
+            {
+                switch (band.ToLower())
+                {
+                    case "carpenter brut":
+                        Console.WriteLine("Very good, very aggressive and clearly an genre leader.");
+                        bandFound = true;
+                        break;
+                    case "scandroid":
+                        Console.WriteLine("First two albums are incredible, but I fell off after that");
+                        bandFound = true;
+                        break;
+                    case "dance with the dead":
+                        Console.WriteLine("First few albums were great. I Fell off after that.");
+                        bandFound = true;
+                        break;
+                    case "twrp":
+                        Console.WriteLine("Amazing Live, Very Talented Musicians.");
+                        bandFound = true;
+                        break;
+                    case "magic sword":
+                        Console.WriteLine("They put on a great live show.");
+                        bandFound = true;
+                        break;
+                    case "michael oakley":
+                        Console.WriteLine("Pretty good, very layered music, Reminds me of George Michael");
+                        bandFound = true;
+                        break;
+                    case "lazerhawk":
+                        Console.WriteLine("Music has too much vamping for my tastes.");
+                        bandFound = true;
+                        break;
+                    case "timecop1983":
+                        Console.WriteLine("Never Listened to their stuff much");
+                        bandFound = true;
+                        break;
+                    case "d.notive":
+                        Console.WriteLine("Pretty good, some catchy stuff for an indie synthwave artist. Personable as well.");
+                        bandFound = true;
+                        break;
+                    case "jamievx":
+                        Console.WriteLine("Another good indie synthwave artist I discovered through bandcamp.");
+                        bandFound = true;
+                        break;
+                    default:
+                        Console.WriteLine("Band not in the list. \n");
+                        break;
+                }
+            }
+        } while (!bandFound);
+        Console.ReadKey();
+    }
+}
+
+
