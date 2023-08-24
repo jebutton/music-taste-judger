1cd7bb2ea5987404ad0880b1a02ff7c5486974a5
jebutton <jacqueline@jebutton.com>
2023-08-24 10:17:39 -0700

    0.002.0 Updated README.md and added MusicalArtist class + dict lookup.

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # music-taste-judger
 
+## Version
+Version 0.002.0
 ## Description
 This is just a silly project I'm working on as I learn C# for my job, posted publicly with permission of my boss. 
 I wanted to create something that isn't just an isolated language exercise when studying C#, especially since I need to learn unit testing in NUnit.
@@ -8,6 +10,10 @@ This tool is designed to judge your choice of Synthwave artist. I may expand it 
 
 ***
 ## Eventual Goals:
-- Implemening more Object Oriented Code like defining a class to represeent an artist.
 - Loading JSON or XML that artist definitions are stored in.
 - Writing tests for the tool using NUnit.
+
+## Changelog
+- 0.002.0 Created MusicalArtist class to hold information about an artist.
+- 0.002.0 Replaced the big switch statement that was used for lookup of artists into a dictionary search.
+- 0.002.0 Using the MusicalArtist class, creasted a methood to generate enough sample artists for development.
--- music-taste-judger/music-taste-judger/Program.cs
+++ music-taste-judger/music-taste-judger/Program.cs
@@ -1,76 +1,122 @@
-﻿/*
- * This is a program created by Jacqueline Button 
- * to allow her to work on implementing features of C#
- * into a working program that isn't just a set of individual
- * Exercises. This code will be improved overtime as she learns more.
- */
+﻿using System.CodeDom.Compiler;
 using static System.Console;
-class Program
+namespace MusicTasteJudger
 {
-    public static void Main()
+    /// <summary>
+    /// This is a program to judge someone's taste in synthwave music via an interactive console app.
+    /// </summary>
+    /// <remarks>
+    /// This is a program created by Jacqueline Button 
+    /// to allow her to work on implementing features of C# into a working program
+    /// that isn't just a set of individual Exercises. 
+    /// This code will be improved overtime as she learns more.
+    /// </remarks>
+    class Program
     {
-        WriteLine("Hello Cyberpunk Boy, Girl, or Hacker");
-        bool bandFound = false;
-        do
+        public static void Main()
         {
-            Write("Please Enter A Good Synthwave Artist: ");
-            string? band = ReadLine();
-            if (band == null)
+            WriteLine("Hello Cyberpunk Boy, Girl, or Hacker");
+            bool bandFound = false;
+            // Generate the list of artists for Testing.
+            Dictionary<string, MusicalArtist> artists = GenerateArtists();
+            do
             {
-                WriteLine("Response is empty or null");
-            }
-            else
-            {
-                switch (band.ToLower())
+                Write("Please Enter A Good Synthwave Artist: ");
+                string? band = ReadLine();
+                if (band == null)
                 {
-                    case "carpenter brut":
-                        Console.WriteLine("Very good, very aggressive and clearly an genre leader.");
-                        bandFound = true;
-                        break;
-                    case "scandroid":
-                        Console.WriteLine("First two albums are incredible, but I fell off after that");
-                        bandFound = true;
-                        break;
-                    case "dance with the dead":
-                        Console.WriteLine("First few albums were great. I Fell off after that.");
-                        bandFound = true;
-                        break;
-                    case "twrp":
-                        Console.WriteLine("Amazing Live, Very Talented Musicians.");
-                        bandFound = true;
-                        break;
-                    case "magic sword":
-                        Console.WriteLine("They put on a great live show.");
-                        bandFound = true;
-                        break;
-                    case "michael oakley":
-                        Console.WriteLine("Pretty good, very layered music, Reminds me of George Michael");
-                        bandFound = true;
-                        break;
-                    case "lazerhawk":
-                        Console.WriteLine("Music has too much vamping for my tastes.");
-                        bandFound = true;
-                        break;
-                    case "timecop1983":
-                        Console.WriteLine("Never Listened to their stuff much");
-                        bandFound = true;
-                        break;
-                    case "d.notive":
-                        Console.WriteLine("Pretty good, some catchy stuff for an indie synthwave artist. Personable as well.");
-                        bandFound = true;
-                        break;
-                    case "jamievx":
-                        Console.WriteLine("Another good indie synthwave artist I discovered through bandcamp.");
-                        bandFound = true;
-                        break;
-                    default:
-                        Console.WriteLine("Band not in the list. \n");
-                        break;
+                    WriteLine("Response is empty or null");
                 }
+                else
+                {
+                    try
+                    {
+                        MusicalArtist searchResult = artists[band.ToLower()];
+                        WriteLine(searchResult.GetFullReview());
+                        bandFound = true;
+                    }
+                    catch (KeyNotFoundException)
+                    {
+                        WriteLine("Artist / Band is not found. Try again.");
+                    }
+                }
+            } while (!bandFound);
+            Console.ReadKey();
+        }
+        /// <summary>
+        /// This is a temporary method to generate a dictionary of artists. 
+        /// Eventually this will be replaced by pulling the artists from a file.
+        /// </summary>
+        /// <returns>A Dictionary Collection of MusicalArtist objects with a string key representing the band name.</returns>
+        private static Dictionary<string, MusicalArtist> GenerateArtists()
+        {
+            Dictionary<string, MusicalArtist> artists = new ();
+            try
+            {
+                artists.Add("carpenter brut", new MusicalArtist("Carpenter Brut", 9.0m, "Very good, very aggressive and clearly an genre leader."));
+                artists.Add("scandroid", new MusicalArtist("Scandroid", 8.0m, "First two albums are incredible, but I fell off after that"));
+                artists.Add("dance with the dead", new MusicalArtist("Dance With The Dead", 8.0m, "First few albums were great. I Fell off after that."));
+                artists.Add("twrp", new MusicalArtist("TWRP", 9.0m, "Amazing Live, Very Talented Musicians."));
+                artists.Add("magic sword", new MusicalArtist("Magic Sword", 6.0m, "They put on a surprisingly good live show."));
+                artists.Add("michael oakley", new MusicalArtist("Michael Oakley", 7.0m, "Has some really good tracks, Reminds me of George Michael."));
+                artists.Add("lazerhawk", new MusicalArtist("LazerHawk", 4.0m, "Not a big fan of their music."));
+                artists.Add("timecop1983", new MusicalArtist("TimeCop1983", 1.0m, "Never listened to their stuff much."));
+                artists.Add("d.notive", new MusicalArtist("d.notive", 9.0m, "Pretty good, some catchy stuff for an indie synthwave artist. Personable as well."));
+                artists.Add("jamievx", new MusicalArtist("JAMIEvx", 8.0m, "Another good indie synthwave artist I discovered through bandcamp."));
             }
-        } while (!bandFound);
-        Console.ReadKey();
+            catch (ArgumentException)
+            {
+                WriteLine("There is a duplicate key in your list.");
+            }
+            return artists;
+        } 
+    }
+    /// <summary>
+    /// This is a class to repressent a Musical Artist. It contains some key values about them.
+    /// </summary>
+    public class MusicalArtist
+    {
+        private string? artistName;
+        private decimal artistScore;
+        private string? artistReview;
+        /// <summary>
+        /// Constructor with all private fields.
+        /// </summary>
+        /// <param name="name">The Name of the Artist</param>
+        /// <param name="score">The Score of the Artist</param>
+        /// <param name="review"> The Review of the Artist</param>
+        public MusicalArtist(string name, decimal score, string review) { 
+            artistName = name;
+            artistScore = score;
+            artistReview = review;
+        }
+        /// <summary>
+        /// Constructor without the review.
+        /// </summary>
+        /// <param name="name">The Name of the Artist</param>
+        /// <param name="score">The Score of the Artist</param>
+        public MusicalArtist(string name, decimal score)
+        {
+            artistName = name;
+            artistScore = score;
+            GenerateReview();
+        }
+        /// <summary>
+        /// For expansion later. This will eventually generate a review
+        /// for artists that I don't know about.
+        /// </summary>
+        private void GenerateReview()
+        {
+            artistReview = "This will be Expanded Later";
+        }
+        /// <summary>
+        /// Returns a well structured review.
+        /// </summary>
+        /// <returns>A string containing a review.</returns>
+        public string GetFullReview()
+        {
+            string fullReview = $"The artist {artistName} has an index score of {artistScore} and my opinion of them is: {artistReview}";
+            return fullReview;
+        }
     }
 }
-
-
